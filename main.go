d88b4a344b0bff7206c84b4fa2de7e4412a1fdd6
杜娟 <wudujuan@tinyverse.space>
2023-03-04 09:32:39 +0800

    去掉跨域配置，只需在nginx配置

--- main.go
+++ main.go
@@ -9,16 +9,6 @@ import (
 )
 
 func main() {
-	// env, _ := config.String("runmode")
-	// logs.Info("adapterName:", env)
-	// configFilePath, err := filepath.Abs("conf/" + env + ".conf")
-	// if err != nil {
-	// 	logs.Error("configFilePathError", err)
-	// }
-	// err = beego.LoadAppConfig("ini", configFilePath)
-	// if err != nil {
-	// 	logs.Error("loadConfigFileError", err)
-	// }
 
 	//数据库初始化
 	utils.InitMySQL()
